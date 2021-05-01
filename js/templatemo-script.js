4ea7cdd752b7f04127d5c1886cbdf68563d1d444
Priyaraj17 <priyaraj.17@outlook.com>
2021-05-01 21:40:38 +0530

    Responsive navbar updated

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,61 +1,63 @@
 // https://codepen.io/AmruthPillai/pen/axvqB
 (function ($) {
-    $.fn.extend({
-        rotaterator: function (options) {
+  $.fn.extend({
+    rotaterator: function (options) {
+      var defaults = {
+        fadeSpeed: 500,
+        pauseSpeed: 100,
+        child: null,
+      };
 
-            var defaults = {
-                fadeSpeed: 500,
-                pauseSpeed: 100,
-                child: null
-            };
+      var options = $.extend(defaults, options);
 
-            var options = $.extend(defaults, options);
-
-            return this.each(function () {
-                var o = options;
-                var obj = $(this);
-                var items = $(obj.children(), obj);
-                items.each(function () {
-                    $(this).hide();
-                })
-                if (!o.child) {
-                    var next = $(obj).children(':first');
-                } else {
-                    var next = o.child;
-                }
-                $(next).fadeIn(o.fadeSpeed, function () {
-                    $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function () {
-                        var next = $(this).next();
-                        if (next.length == 0) {
-                            next = $(obj).children(':first');
-                        }
-                        $(obj).rotaterator({
-                            child: next,
-                            fadeSpeed: o.fadeSpeed,
-                            pauseSpeed: o.pauseSpeed
-                        });
-                    })
-                });
-            });
+      return this.each(function () {
+        var o = options;
+        var obj = $(this);
+        var items = $(obj.children(), obj);
+        items.each(function () {
+          $(this).hide();
+        });
+        if (!o.child) {
+          var next = $(obj).children(":first");
+        } else {
+          var next = o.child;
         }
-    });
+        $(next).fadeIn(o.fadeSpeed, function () {
+          $(next)
+            .delay(o.pauseSpeed)
+            .fadeOut(o.fadeSpeed, function () {
+              var next = $(this).next();
+              if (next.length == 0) {
+                next = $(obj).children(":first");
+              }
+              $(obj).rotaterator({
+                child: next,
+                fadeSpeed: o.fadeSpeed,
+                pauseSpeed: o.pauseSpeed,
+              });
+            });
+        });
+      });
+    },
+  });
 })(jQuery);
 
 $(document).ready(function () {
+  // Check for video loading complete
+  var video = document.getElementById("tm-welcome-video");
 
-    // Check for video loading complete
-    var video = document.getElementById("tm-welcome-video");
+  video.onloadeddata = function () {
+    $("#tm-video-text-overlay").removeClass("d-none");
+    $("#tm-video-loader").addClass("d-none");
 
-    video.onloadeddata = function() {
-        $('#tm-video-text-overlay').removeClass('d-none');
-        $('#tm-video-loader').addClass('d-none');
+    $("#rotate").rotaterator({
+      fadeSpeed: 1000,
+      pauseSpeed: 300,
+    });
+  };
 
-        $('#rotate').rotaterator({
-            fadeSpeed: 1000,
-            pauseSpeed: 300
-        });
-    }
-
-    // Update year in copyright text
-    document.querySelector('.tm-current-year').textContent = new Date().getFullYear();
+  // Update year in copyright text
+  document.querySelector(
+    ".tm-current-year"
+  ).textContent = new Date().getFullYear();
 });
