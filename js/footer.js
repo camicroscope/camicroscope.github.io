dba25380805b15add43f8649621c6513d1afe702
Birm <birm@rbirm.us>
2023-11-27 12:16:44 -0500

    range copyright, add license, add easter egg

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,3 @@
 const footer = document.getElementsByClassName("footer__content__copyright")[0];
 
-footer.innerHTML = `Copyright © ${new Date().getFullYear()} caMicroscope`;
+footer.innerHTML = `© <a style="color: #ffffff; text-decoration: none;" href="https://github.com/camicroscope/caMicroscope/commit/316bcc66dbddebca8e054a562993ac309cfb5cbc">2012</a> - ${new Date().getFullYear()} caMicroscope developers (BSD License)`;
